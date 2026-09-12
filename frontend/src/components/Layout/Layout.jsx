2f64208f0bce3b3ef4e87e3140ec1a1fe94bf854
Lameck Fernandes <lameck.fernandes@omni-electronica.com>
2026-09-11 23:50:09 -0300

    refactor: Layout sem estado/props de sidebar retraida (menu sempre fixo apos remocao do botao)

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -5,24 +5,12 @@ import Sidebar from './Sidebar.jsx'
 
 export default function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
-  // Sidebar retraida (desktop) - persiste a preferencia do usuario
-  const [sidebarCollapsed, setSidebarCollapsed] = useState(
-    () => localStorage.getItem('sidebarCollapsed') === '1',
-  )
   const location = useLocation()
 
   // Fecha sidebar ao navegar
   React.useEffect(() => {
     setSidebarOpen(false)
   }, [location.pathname])
-
-  const handleToggleCollapse = () => {
-    setSidebarCollapsed((c) => {
-      const next = !c
-      localStorage.setItem('sidebarCollapsed', next ? '1' : '0')
-      return next
-    })
-  }
 
   return (
     <div className="min-h-screen bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text">
@@ -31,8 +19,6 @@ export default function Layout() {
         <Sidebar
           open={sidebarOpen}
           onClose={() => setSidebarOpen(false)}
-          collapsed={sidebarCollapsed}
-          onToggleCollapse={handleToggleCollapse}
         />
         <main className="flex-1 p-4 lg:p-8 min-h-[calc(100vh-4rem)]">
           <Outlet />
